9bf4981046cf02d87a3896fac557a7b9e78195c9
piyushgarg22 <pgarg2020@gmail.com>
2020-05-09 17:36:21 +0530

    passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 var FileStore = require("session-file-store")(session);
+var passport = require("passport");
+var authenticate = require("./authenticate");
 
 var dishRouter = require("./routes/dishRouter");
 var leaderRouter = require("./routes/leaderRouter");
@@ -45,25 +47,21 @@ app.use(
     store: new FileStore(),
   })
 );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", userRouter);
 
 // auth
 function auth(req, res, next) {
-  console.log(req.session);
-
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated !");
     err.status = 401;
     return next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated");
-      err.status = 401;
-      return next(err);
-    }
+    next();
   }
 }
 
